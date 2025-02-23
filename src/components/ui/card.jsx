d7a4adb0dad53cd978d4ee6fb894fd53ada30ad6
Patycaciquinho <patricia.caciquinho@lbca.com.br>
2025-02-23 12:34:03 -0300

    Create card.jsx

---
+++ src/components/ui/card.jsx
@@ -0,0 +1,35 @@
+
+// src/components/ui/card.jsx
+import React from 'react';
+
+export const Card = ({ children, className = '', ...props }) => {
+  return (
+    <div className={`bg-white rounded-lg shadow-md ${className}`} {...props}>
+      {children}
+    </div>
+  );
+};
+
+export const CardHeader = ({ children, className = '', ...props }) => {
+  return (
+    <div className={`p-4 border-b ${className}`} {...props}>
+      {children}
+    </div>
+  );
+};
+
+export const CardTitle = ({ children, className = '', ...props }) => {
+  return (
+    <h3 className={`text-xl font-semibold ${className}`} {...props}>
+      {children}
+    </h3>
+  );
+};
+
+export const CardContent = ({ children, className = '', ...props }) => {
+  return (
+    <div className={`p-4 ${className}`} {...props}>
+      {children}
+    </div>
+  );
+};
