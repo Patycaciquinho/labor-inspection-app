745acf9e9ac381a13397c200edca3f1162286d24
Patycaciquinho <patricia.caciquinho@lbca.com.br>
2025-02-23 12:32:51 -0300

    Create button.jsx

---
+++ src/components/ui/button.jsx
@@ -0,0 +1,13 @@
+// src/components/ui/button.jsx
+import React from 'react';
+
+export const Button = ({ children, className = '', ...props }) => {
+  return (
+    <button
+      className={`px-4 py-2 rounded-md text-white ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
